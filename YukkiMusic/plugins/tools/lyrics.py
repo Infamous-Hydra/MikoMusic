21f61a9fa94474816216256971030514445393b0
Infamous-Hydra <116187035+Infamous-Hydra@users.noreply.github.com>
2023-01-23 20:27:56 +0600

    Update lyrics.py

--- YukkiMusic/plugins/tools/lyrics.py
+++ YukkiMusic/plugins/tools/lyrics.py
@@ -1,30 +1,20 @@
-#
-# Copyright (C) 2021-2022 by TeamYukki@Github, < https://github.com/TeamYukki >.
-#
-# This file is part of < https://github.com/TeamYukki/YukkiMusicBot > project,
-# and is released under the "GNU v3.0 License Agreement".
-# Please see < https://github.com/TeamYukki/YukkiMusicBot/blob/master/LICENSE >
-#
-# All rights reserved.
-
 import random
 import re
 import string
 
 import lyricsgenius as lg
 from pyrogram import filters
-from pyrogram.types import (InlineKeyboardButton,
-                            InlineKeyboardMarkup, Message)
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
 
-from config import BANNED_USERS, lyrical
-from strings import get_command
 from YukkiMusic import app
 from YukkiMusic.utils.decorators.language import language
+from config import BANNED_USERS, lyrical
+from strings import get_command
 
 ###Commands
 LYRICS_COMMAND = get_command("LYRICS_COMMAND")
 
-api_key = "Vd9FvPMOKWfsKJNG9RbZnItaTNIRFzVyyXFdrGHONVsGqHcHBoj3AI3sIlNuqzuf0ZNG8uLcF9wAd5DXBBnUzA"
+api_key = "JVv8pud-25QRBYyRwcH34AlAygySsSAU3owRNGBw6hXO96x0JiTMn-3R4PvsjcTf"
 y = lg.Genius(
     api_key,
     skip_non_songs=True,
@@ -34,9 +24,7 @@ y = lg.Genius(
 y.verbose = False
 
 
-@app.on_message(
-    filters.command(LYRICS_COMMAND) & ~filters.edited & ~BANNED_USERS
-)
+@app.on_message(filters.command(LYRICS_COMMAND) & ~filters.edited & ~BANNED_USERS)
 @language
 async def lrsearch(client, message: Message, _):
     if len(message.command) < 2:
@@ -46,9 +34,7 @@ async def lrsearch(client, message: Message, _):
     S = y.search_song(title, get_full_info=False)
     if S is None:
         return await m.edit(_["lyrics_3"].format(title))
-    ran_hash = "".join(
-        random.choices(string.ascii_uppercase + string.digits, k=10)
-    )
+    ran_hash = "".join(random.choices(string.ascii_uppercase + string.digits, k=10))
     lyric = S.lyrics
     if "Embed" in lyric:
         lyric = re.sub(r"\d*Embed", "", lyric)
