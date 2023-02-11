3ccb0ed7a9498d7f2413b6470223aa903386573d
Infamous-Hydra <116187035+Infamous-Hydra@users.noreply.github.com>
2023-02-12 03:34:12 +0600

    Delete welcome.py

--- YukkiMusic/plugins/welcome.py
+++
@@ -1,7 +0,0 @@
-CHAT_LOGS = -1001854146871
-
-elif new_mem.id == bot.id:
-                gcid = chat.id
-                gcname = chat.username
-                bot.send_message(CHAT_LOGS, f"New group: @{gcname}\nGroup ID: {gcid}")
-     
