9209ba0fa9811923a5f058f15863aec00c5d89fc
Infamous-Hydra <faisal.hasan2054@gmail.com>
2023-02-27 15:15:32 +0000

    Smash

--- YukkiMusic/plugins/Red7.py
+++ YukkiMusic/plugins/Red7.py
@@ -1,16 +1,18 @@
 """ Team Red7 """
-from .. import app as Red7
-from pyrogram.types import Message
 from pyrogram import filters
+from pyrogram.types import Message
+
+from .. import app as Red7
 
 try:
-  from RiZoeLX.functions import Red7_Watch
+    from RiZoeLX.functions import Red7_Watch
 except:
-  import os
-  os.system("pip3 install pyRiZoeLX")
-  from RiZoeLX.functions import Red7_Watch
+    import os
+
+    os.system("pip3 install pyRiZoeLX")
+    from RiZoeLX.functions import Red7_Watch
+
 
 @Red7.on_message(filters.group & filters.all)
 async def Red7_Scanner(_, message: Message):
     await Red7_Watch(Red7, message)
-
--- YukkiMusic/utils/inline/play.py
+++ YukkiMusic/utils/inline/play.py
@@ -50,11 +50,7 @@ def stream_markup_timer(_, videoid, chat_id, played, dur):
                 callback_data=f"PanelMarkup {videoid}|{chat_id}",
             ),
         ],
-        [
-            InlineKeyboardButton(
-                text=_["CLOSEMENU_BUTTON"], callback_data="close"
-            )
-        ],
+        [InlineKeyboardButton(text=_["CLOSEMENU_BUTTON"], callback_data="close")],
     ]
     return buttons
 
@@ -73,9 +69,7 @@ def telegram_markup_timer(_, chat_id, played, dur):
                 text=_["PL_B_3"],
                 callback_data=f"PanelMarkup None|{chat_id}",
             ),
-            InlineKeyboardButton(
-                text=_["CLOSEMENU_BUTTON"], callback_data="close"
-            ),
+            InlineKeyboardButton(text=_["CLOSEMENU_BUTTON"], callback_data="close"),
         ],
     ]
     return buttons
@@ -96,11 +90,7 @@ def stream_markup(_, videoid, chat_id):
                 callback_data=f"PanelMarkup None|{chat_id}",
             ),
         ],
-        [
-            InlineKeyboardButton(
-                text=_["CLOSEMENU_BUTTON"], callback_data="close"
-            )
-        ],
+        [InlineKeyboardButton(text=_["CLOSEMENU_BUTTON"], callback_data="close")],
     ]
     return buttons
 
@@ -112,9 +102,7 @@ def telegram_markup(_, chat_id):
                 text=_["PL_B_3"],
                 callback_data=f"PanelMarkup None|{chat_id}",
             ),
-            InlineKeyboardButton(
-                text=_["CLOSEMENU_BUTTON"], callback_data="close"
-            ),
+            InlineKeyboardButton(text=_["CLOSEMENU_BUTTON"], callback_data="close"),
         ],
     ]
     return buttons
@@ -189,9 +177,7 @@ def livestream_markup(_, videoid, user_id, mode, channel, fplay):
 ## Slider Query Markup
 
 
-def slider_markup(
-    _, videoid, user_id, query, query_type, channel, fplay
-):
+def slider_markup(_, videoid, user_id, query, query_type, channel, fplay):
     query = f"{query[:20]}"
     buttons = [
         [
@@ -240,9 +226,7 @@ def panel_markup_1(_, videoid, chat_id):
             InlineKeyboardButton(
                 text="‣‣I sᴋɪᴘ", callback_data=f"ADMIN Skip|{chat_id}"
             ),
-            InlineKeyboardButton(
-                text="▢ sᴛᴏᴘ", callback_data=f"ADMIN Stop|{chat_id}"
-            ),
+            InlineKeyboardButton(text="▢ sᴛᴏᴘ", callback_data=f"ADMIN Stop|{chat_id}"),
         ],
         [
             InlineKeyboardButton(
@@ -265,9 +249,7 @@ def panel_markup_1(_, videoid, chat_id):
 def panel_markup_2(_, videoid, chat_id):
     buttons = [
         [
-            InlineKeyboardButton(
-                text="🔇 ᴍᴜᴛᴇ", callback_data=f"ADMIN Mute|{chat_id}"
-            ),
+            InlineKeyboardButton(text="🔇 ᴍᴜᴛᴇ", callback_data=f"ADMIN Mute|{chat_id}"),
             InlineKeyboardButton(
                 text="🔊 ᴜɴᴍᴜᴛᴇ",
                 callback_data=f"ADMIN Unmute|{chat_id}",
@@ -278,9 +260,7 @@ def panel_markup_2(_, videoid, chat_id):
                 text="🔀 sʜᴜғғʟᴇ",
                 callback_data=f"ADMIN Shuffle|{chat_id}",
             ),
-            InlineKeyboardButton(
-                text="🔁 ʟᴏᴏᴘ", callback_data=f"ADMIN Loop|{chat_id}"
-            ),
+            InlineKeyboardButton(text="🔁 ʟᴏᴏᴘ", callback_data=f"ADMIN Loop|{chat_id}"),
         ],
         [
             InlineKeyboardButton(
